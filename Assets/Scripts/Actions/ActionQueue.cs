f46814c91cda3ec3c24c4354bff16f52f37966e2
Jinp-He <lofnheart@gmail.com>
2021-10-15 13:47:04 +0800

    Final push to main branch

--- Assets/Scripts/Actions/ActionQueue.cs
+++ Assets/Scripts/Actions/ActionQueue.cs
@@ -35,7 +35,7 @@ namespace ShadowBringer
             {
                 currAction = actionQueue.Peek();
                 currAction.Enter();
-                currAction.Execute();
+                currAction.Execute();  
                 actionQueue.Dequeue(); 
             }
             else
